d7c970e3ba79bd7afa2fba996998a50d0bc323e7
Nik1337 <56128196+Nik1337@users.noreply.github.com>
2022-09-20 15:15:05 +0200

    updated dependencies; added getters & setters

--- src/main/java/bowling/BowlingLine.java
+++ src/main/java/bowling/BowlingLine.java
@@ -2,9 +2,27 @@ package bowling;
 
 public class BowlingLine {
 
-    Frame[] frames;
+    private Frame[] frames;
+    private int totalScore;
+
 
     public BowlingLine (Frame[] frames) {
 
+    }
+
+    public Frame[] getFrames() {
+        return frames;
+    }
+
+    public void setFrames(Frame[] frames) {
+        this.frames = frames;
+    }
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(int totalScore) {
+        this.totalScore = totalScore;
     }
 }
--- src/main/java/bowling/Frame.java
+++ src/main/java/bowling/Frame.java
@@ -1,14 +1,55 @@
 package bowling;
 
 public class Frame {
-
     private int firstThrow;
     private int secondThrow;
+    private int frameTotal;
     private boolean isStrike;
     private boolean isSpare;
 
     public Frame(String roll) {
 
     }
+
+    public int getFirstThrow() {
+        return firstThrow;
+    }
+
+    public void setFirstThrow(int firstThrow) {
+        this.firstThrow = firstThrow;
+    }
+
+    public int getSecondThrow() {
+        return secondThrow;
+    }
+
+    public void setSecondThrow(int secondThrow) {
+        this.secondThrow = secondThrow;
+    }
+
+    public int getFrameTotal() {
+        return frameTotal;
+    }
+
+    public void setFrameTotal(int frameTotal) {
+        this.frameTotal = frameTotal;
+    }
+
+    public boolean isStrike() {
+        return isStrike;
+    }
+
+    public void setStrike(boolean strike) {
+        isStrike = strike;
+    }
+
+    public boolean isSpare() {
+        return isSpare;
+    }
+
+    public void setSpare(boolean spare) {
+        isSpare = spare;
+    }
+
 
 }
