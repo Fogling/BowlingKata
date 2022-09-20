70e06346bd3a4ec13b5fbdca33ca90abe7815d7c
Nik1337 <56128196+Nik1337@users.noreply.github.com>
2022-09-20 13:05:15 +0200

    added class structure

---
+++ src/main/java/bowling/BowlingLine.java
@@ -0,0 +1,6 @@
+package bowling;
+
+public class BowlingLine {
+
+    Frame[] frames;
+}
---
+++ src/main/java/bowling/Frame.java
@@ -0,0 +1,10 @@
+package bowling;
+
+public class Frame {
+
+    private int firstThrow;
+    private int secondThrow;
+    private boolean isStrike;
+    private boolean isSpare;
+
+}
